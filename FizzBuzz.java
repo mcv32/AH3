776c5f45031cdbb6bee79a3ef10d74e885a6ef07
Villatoro <marcus.villatoro@fiserv.com>
2023-10-16 16:44:29 -0400

    FizzBuzz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -23,17 +23,14 @@ public class FizzBuzz {
 
     public static void recursiveFizzBuzz(int num){
         if(num == 0) return;
+        recursiveFizzBuzz(num-1);
         if(num % 3 == 0 && num % 5 == 0){
-            recursiveFizzBuzz(num-1);
             System.out.println("FizzBuzz");
         }else if(num % 3 == 0){
-            recursiveFizzBuzz(num-1);
             System.out.println("Fizz");
         }else if(num % 5 == 0){
-            recursiveFizzBuzz(num-1);
             System.out.println("Buzz");
         }else{
-            recursiveFizzBuzz(num-1);
             System.out.println(num);
         }
     }
